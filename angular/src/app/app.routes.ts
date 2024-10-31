b1b4eae3bb0c3959d4b3ad8a68e30ef0ba927fb4
ahmedAlnono <111354914+ahmedAlnono@users.noreply.github.com>
2024-10-31 08:22:18 +0200

    Update app.routes.ts
    delete main page as dashboard
    i want to make main page custom main page

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -7,10 +7,6 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './pages/login/login.component';
 export const routes: Routes = [
   {
-    path: '',
-    component: DashboardComponent,
-  },
-  {
     path: 'chart',
     component: ChartComponent,
   },
