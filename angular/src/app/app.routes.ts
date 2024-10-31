7d1b0f4e5f1073e4d180a24c61a70d36b1109c49
ahmedAlnono <111354914+ahmedAlnono@users.noreply.github.com>
2024-10-31 08:17:50 +0200

    Update app.routes.ts
    add the main page is dashboard

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -7,6 +7,10 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './pages/login/login.component';
 export const routes: Routes = [
   {
+    path: '',
+    component: DashboardComponent,
+  },
+  {
     path: 'chart',
     component: ChartComponent,
   },
